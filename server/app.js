0da0fe0558b065a8a9d85d0c6855b2f1438b8c60
justArale <just1arale@gmail.com>
2024-05-13 16:31:27 +0200

    done day 2

--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = 5005;
+const mongoose = require("mongoose");
+
+const Student = require("./models/Student.model");
+const Cohort = require("./models/Cohort.model");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
+// const cohorts = require("./cohorts.json");
+// const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -14,6 +24,17 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+// Use the CORS middleware with options to allow requests
+// from specific IP addresses and domains.
+/*
+To ensure your CORS middleware is working properly, you should set it before any route or router definition. The easiest way to do this is to place cors as the first middleware right after instantiating the server (const app = express()).
+*/
+app.use(
+  cors({
+    // Add the URLs of allowed origins to this array
+    origin: ["http://localhost:5173", "http://example.com"],
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -28,11 +49,27 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
+  Cohort.find({})
+    .then((cohorts) => {
+      console.log("Retrieved books ->", cohorts);
+      res.json(cohorts);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving books ->", error);
+      res.status(500).json({ error: "Failed to retrieve books" });
+    });
 });
 
 app.get("/api/students", (req, res) => {
-  res.json(students);
+  Student.find({})
+    .then((students) => {
+      console.log("Retrieved books ->", students);
+      res.json(students);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving books ->", error);
+      res.status(500).json({ error: "Failed to retrieve books" });
+    });
 });
 
 // START SERVER
