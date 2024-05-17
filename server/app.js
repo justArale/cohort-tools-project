92b044153852ade1b95b4c9e48d422e20be388d2
justArale <just1arale@gmail.com>
2024-05-17 14:35:05 +0200

    some code cleaning

--- server/app.js
+++ server/app.js
@@ -1,48 +1,18 @@
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const PORT = 5005;
-const mongoose = require("mongoose");
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv").config();
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
-  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
-  .catch((err) => console.log("Error connecting to MongoDB", err));
-
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
+// ℹ️ Connects to the database
+require("./db");
 const { isAuthenticated } = require("./middleware/jwt.middleware");
-// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
 const app = express();
 
-// MIDDLEWARE
-// Research Team - Set up CORS middleware here:
-app.use(
-  cors({
-    // Add the URLs of allowed origins to this array
-    origin: [
-      "http://localhost:5173",
-      "http://example.com",
-      "http://localhost:5005",
-    ],
-  })
-);
-// Use the CORS middleware with options to allow requests
-// from specific IP addresses and domains.
-/*
-To ensure your CORS middleware is working properly, you should set it before any route or router definition. The easiest way to do this is to place cors as the first middleware right after instantiating the server (const app = express()).
-*/
-
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// ROUTES - https://expressjs.com/en/starter/basic-routing.html
-// Devs Team - Start working on the routes here:
-// ...
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config")(app);
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
@@ -67,9 +37,9 @@ app.use("/auth", authRouter);
 const userRouter = require("./routes/user.routes");
 app.use("/api", isAuthenticated, userRouter);
 
+// const userRouter = require(“./routes/user.routes”);
+// app.use(“/”, userRouter);
+
 require("./error-handling")(app);
 
-// START SERVER
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+module.exports = app;
